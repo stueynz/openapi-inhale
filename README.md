0b7c31b04c7e8025f677d1938fbc3376c37cbd61
Stuart McGrigor <McGrigorS@moe.govt.nz>
2020-11-10 23:56:26 +1300

    A new tool, to inhale external  from JSON Schema into OpenAPI spec

---
+++ README.md
@@ -0,0 +1,45 @@
+# openapi-inhale
+
+
+Inhale external `$refs` to a JSON schema into internal `$refs` by importing the matching
+jsonSchema `properties` and `definitions` into the OpenAPI specification.
+
+Only import those `properties` and `definitions` that are actually used by the OpenAPI specification.
+
+Works with OpenAPI 3.0.x definitions and $draft7 JSON Schemas
+
+```
+openapi-inhale.js <oasfile> <jsonfile> [outfile]
+
+Positionals:
+  oasfile   the input openAPI spec
+  jsonfile  the input jsonSchema
+  outfile   the output file
+
+Options:
+  -p, --properties    don't grab properties from the jsonSchema
+                                                      [boolean] [default: false]
+  -d, --definitions   don't grab definitions from the jsonSchema
+                                                      [boolean] [default: false]
+  -i, --importPrefix  import prefix to be added to the front of imported
+                      properties                       [string] [default: "imp"]
+  --maxAliasCount     maximum YAML aliases allowed                [default: 100]
+  --help              Show help                                        [boolean]
+  --version           Show version number                              [boolean]
+
+```
+
+use `--` to separate flags or other array options from following options, i.e.:
+
+```bash
+  openapi-inhale -i create -- openApi.yaml jsonSchema.yaml output.yaml
+```
+
+will resolve the `$refs` in `openApi.yaml` that point to `jsonSchema.yaml`,  using prefix `create` to distinguish the imported `properties`.  `definitions` are imported without a prefix.
+
+
+```bash
+  openapi-inhale -d -i patch -- openApi.yaml jsonSchema.yaml output.yaml
+```
+
+will resolve the `$refs` in `openApi.yaml` that point to `jsonSchema.yaml`,  with prefix `patch` without importing the definitions (because they've already been imported previously).  NOTE: properties in `jsonSchema.yaml` are imported.
---
+++ openapi-inhale.js
@@ -0,0 +1,157 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const yaml = require('yaml');
+const recurse = require('reftools/lib/recurse.js').recurse;
+
+const {strOptions} = require('yaml/types');
+
+let argv = require('yargs')
+    .usage('$0 <oasfile> <jsonfile> [outfile]',false,(yargs) => {
+      yargs
+      .positional('oasfile', { describe: 'the input openAPI spec' })
+      .positional('jsonfile',{ describe: 'the input jsonSchema' })
+      .positional('outfile', { describe: 'the output file' })
+    })
+    .strict()
+
+    .boolean('p')
+    .alias('p', 'properties')
+    .default('p', false)
+    .describe('p','don\'t grab properties from the jsonSchema')
+
+    .boolean('d')
+    .alias('d', 'definitions')
+    .default('d', false)
+    .describe('d','don\'t grab definitions from the jsonSchema')
+
+    .string('i')
+    .alias('i', 'importPrefix')
+    .default('i', 'imp')
+    .describe('i', 'import prefix to be added to the front of imported properties')
+
+    .default('maxAliasCount',100)
+    .describe('maxAliasCount','maximum YAML aliases allowed')
+    .help()
+    .version()
+    .argv;
+
+let s = fs.readFileSync(argv.oasfile,'utf8');
+let oasSpec = yaml.parse(s, {maxAliasCount: argv.maxAliasCount});
+
+let j = fs.readFileSync(argv.jsonfile, 'utf8');
+let jsonSpec = yaml.parse(j, {maxAliasCount: argv.maxAliasCount});
+
+//  $refs to #/definitions,  #/properties or argv.jsonfile#/properties need
+//  to be converted into $refs to #/components/schemas..
+let fixupRefs = (obj) => {
+    recurse(obj, {}, function(obj, key, state) {
+
+        // imported definitions/properties may have $refs to other definitions...
+        if(obj.$ref && obj.$ref.startsWith('#/definitions/')) {
+            obj.$ref = '#/components/schemas/' + obj.$ref.substring(14);
+            
+            // sibling annotations are not -strict
+            if(obj.description)
+                delete obj.description;
+            if(obj.title)
+                delete obj.title;
+        }
+
+        // imported definitions/properties may have $refs to other properties...
+        if(obj.$ref && obj.$ref.startsWith('#/properties/')) {
+            obj.$ref = '#/components/schemas/' + obj.$ref.substring(13);
+            
+            // sibling annotations are not -strict
+            if(obj.description)
+                delete obj.description;
+            if(obj.title)
+                delete obj.title;
+        }
+
+        // original oasSpec will have $refs to the argv.jsonfile#/properties
+        if(obj.$ref && obj.$ref.startsWith(argv.jsonfile + '#/properties/')) {
+            obj.$ref = '#/components/schemas/' + argv.importPrefix + obj.$ref.substring(argv.jsonfile.length+13);
+        }
+    });
+
+    return obj;
+};
+
+// Turn $refs to argv.jsonfile into internal $refs...
+Object.keys(oasSpec.components.schemas).forEach((key, ndx, keys) => {
+    oasSpec.components.schemas[key] = fixupRefs(oasSpec.components.schemas[key]);
+});
+
+// maybe, grab the jsonSchema Properties
+if(! argv.properties) {
+    Object.keys(jsonSpec.properties).forEach((key, ndx, keys) => {
+        oasSpec.components.schemas[argv.importPrefix+key] = fixupRefs(jsonSpec.properties[key]);
+    });
+}
+
+// maybe, grab the jsonSchema Definitions
+if(! argv.definitions) {
+    Object.keys(jsonSpec.definitions).forEach((key, ndx, keys) => {
+        oasSpec.components.schemas[key] = fixupRefs(jsonSpec.definitions[key]);
+    });
+}
+
+let usedSchemas = [];
+
+// check oasSpec.paths to get the set of components/schemas that're actually used...
+Object.keys(oasSpec.paths).forEach((key, ndx, keys) => {
+
+    recurse(oasSpec.paths[key], {}, (obj, key, state) => {
+
+        // go looking for $refs
+        if(obj.$ref && obj.$ref.startsWith('#/components/schemas/')) {
+
+            if(! usedSchemas.includes(obj.$ref.substring(21))) {
+                usedSchemas.push(obj.$ref.substring(21));
+            }
+        }
+    });
+});
+
+
+// go looking for $refs in the set of usedSchemas...and augment the set of usedSchemas as appropriate
+var additions;
+do {    // keep going until we don't add any more schemas; cycles will break this.
+    additions = 0;
+    usedSchemas.forEach((key, ndx, keys) => {
+
+        recurse(oasSpec.components.schemas[key], {}, (obj, key, state) => {
+
+            // go looking for $refs
+            if(obj.$ref && obj.$ref.startsWith('#/components/schemas/')) {
+
+                if(! usedSchemas.includes(obj.$ref.substring(21))) {
+                    usedSchemas.push(obj.$ref.substring(21));
+                    additions++;
+                }
+            }
+        });
+    });
+} while (additions > 0);
+
+// prune the set of schemas in the original oasSpec, to be only the ones we're using...
+Object.keys(oasSpec.components.schemas).forEach((key, ndx, keys) => {
+
+    if(! usedSchemas.includes(key)) {
+        delete oasSpec.components.schemas[key];
+    }
+});
+
+// the result string...
+s = yaml.stringify(oasSpec);
+
+// write it to the correct place...
+if (argv.outfile) {
+    fs.writeFileSync(argv.outfile,s,'utf8');
+}
+else {
+    console.log(s);
+}
